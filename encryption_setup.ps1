ede538bcc4a37807603b343e5af5abc5e172b02a
irankon <irankon@protonmail.com>
2017-04-15 17:42:01 +0100

    Encryption Scripts
    MS script
    Own steps

---
+++ encryption_setup.ps1
@@ -0,0 +1,20 @@
+﻿#Login to Azure and resource manager
+Add-AzureAccount
+Login-AzureRmAccount
+
+#Just in case you have multiple subscriptions check which one you're working in
+Get-AzureSubscription
+
+#If you need to select your test subscription use:
+#Set-AzureSubscription -SubscriptionName <name>
+
+#First the resource group
+$RGName = "security-rg"
+$Location = "North Europe"
+
+#Create an encrypted storage account for Azure Security Center
+#Currently encryption is only available for Azure Blob
+New-AzureRmStorageAccount -ResourceGroupName $RGName -AccountName "securitylogstr" -Location $Location -Type "Standard_LRS" -EnableEncryptionService Blob
+
+#https://docs.microsoft.com/en-us/azure/storage/storage-security-guide#encryption-at-rest
+#https://docs.microsoft.com/en-us/azure/security-center/security-center-intro
